df2896fd805a99d3dc3f2e75438a0eefd29b00df
Nikhil Majhajan <nikhilmahajan937@.gmail.com>
2023-06-10 13:11:10 +0530

    first commit

---
+++ GitProject/src/com/velocity/CGitExample.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class CGitExample {
+
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer> ai=new ArrayList<>();
+		ai.add(10);
+		ai.add(20);
+		ai.add(30);
+		System.out.println(ai);
+		
+		
+
+	}
+
+}
